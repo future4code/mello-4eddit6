73d43ba763ebda80eab967f21e069fbc2d35e6c1
Luiz <luizmitsuru@gmail.com>
2020-07-10 14:23:54 -0300

    Botão curtir e descurtir funcionando sem atualizar sozinho

--- 4eddit/src/Components/PostsCard/index.js
+++ 4eddit/src/Components/PostsCard/index.js
@@ -1,52 +1,135 @@
-import React, { useEffect, useState} from 'react'
-import { CardContainer, InfoBars, ContentContainer, BottomDetails } from '../../Styled'
-import { ArrowUpward, ArrowDownward } from '@material-ui/icons'
-import { Typography } from '@material-ui/core'
-import { useHistory, useParams } from 'react-router-dom'
-import Skeleton from 'react-loading-skeleton'
+import React, { useEffect, useState } from "react";
+import {
+  CardContainer,
+  InfoBars,
+  ContentContainer,
+  BottomDetails,
+} from "../../Styled";
+import { ArrowUpward, ArrowDownward } from "@material-ui/icons";
+import { Typography } from "@material-ui/core";
+import { useHistory, useParams } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+import axios from "axios";
 
 const PostCard = (props) => {
-
-  const postInfo = props.post
-  const history = useHistory()
-  const { postId } = useParams()
-  const [ isLoading, setIsLoading ] = useState(true)
+  const postInfo = props.post;
+  const history = useHistory();
+  const { postId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
-    return () => clearTimeout(timer)
-
-  },[])
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
   const goToDetails = (id) => {
-    history.push(`/posts/${id}`)
-  }
+    history.push(`/posts/${id}`);
+  };
 
-	return(
-		<CardContainer>
-			<InfoBars>
-        <Typography variant='h5'>{ isLoading  ? <Skeleton height={'auto'} width={200} /> : postInfo.username}</Typography>
-			</InfoBars>
-			<ContentContainer>
-        <Typography variant='h5'>{ isLoading  ? <Skeleton width={200} /> : postInfo.title}</Typography>
-        <Typography variant='h6'>{ isLoading  ? <Skeleton width={200} /> : postInfo.text}</Typography>
-			</ContentContainer>
+  const handleDirectionUp = (id) => {
+    const Headers = { headers: { Authorization: token } };
+    const body = {
+      direction: 1,
+    };
+    axios
+      .put(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labEddit/posts/${id}/vote`,
+        body,
+        Headers
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  const handleDirectionDown = (id) => {
+    const Headers = { headers: { Authorization: token } };
+    const body = {
+      direction: -1,
+    };
+    axios
+      .put(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labEddit/posts/${id}/vote`,
+        body,
+        Headers
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  return (
+    <CardContainer>
       <InfoBars>
-				<BottomDetails>
-					<ArrowUpward />
-          <Typography>{ isLoading  ? <Skeleton height={16} width={20} /> : postInfo.votesCount}</Typography>
-					<ArrowDownward />
-				</BottomDetails>
-				<BottomDetails>
-          { postId ? <Typography>{ isLoading  ? <Skeleton height={16} width={20} /> : postInfo.commentsCount} comentários</Typography>:
-          <Typography onClick={() => goToDetails(postInfo.id)}>{ isLoading  ? <Skeleton height={16} width={20} /> :postInfo.commentsCount} comentários</Typography>
-          }
-				</BottomDetails>
-			</InfoBars>
-		</CardContainer>
-	)
-}
+        <Typography variant="h5">
+          {isLoading ? (
+            <Skeleton height={"auto"} width={200} />
+          ) : (
+            postInfo.username
+          )}
+        </Typography>
+      </InfoBars>
+      <ContentContainer>
+        <Typography variant="h5">
+          {isLoading ? <Skeleton width={200} /> : postInfo.title}
+        </Typography>
+        <Typography variant="h6">
+          {isLoading ? <Skeleton width={200} /> : postInfo.text}
+        </Typography>
+      </ContentContainer>
+      <InfoBars>
+        <BottomDetails>
+          <ArrowUpward
+            onClick={() => {
+              handleDirectionUp(postInfo.id);
+            }}
+          />
+          <Typography>
+            {isLoading ? (
+              <Skeleton height={16} width={20} />
+            ) : (
+              postInfo.votesCount
+            )}
+          </Typography>
+          <ArrowDownward
+            onClick={() => {
+              handleDirectionDown(postInfo.id);
+            }}
+          />
+        </BottomDetails>
+        <BottomDetails>
+          {postId ? (
+            <Typography>
+              {isLoading ? (
+                <Skeleton height={16} width={20} />
+              ) : (
+                postInfo.commentsCount
+              )}{" "}
+              comentários
+            </Typography>
+          ) : (
+            <Typography onClick={() => goToDetails(postInfo.id)}>
+              {isLoading ? (
+                <Skeleton height={16} width={20} />
+              ) : (
+                postInfo.commentsCount
+              )}{" "}
+              comentários
+            </Typography>
+          )}
+        </BottomDetails>
+      </InfoBars>
+    </CardContainer>
+  );
+};
 
-export default PostCard
+export default PostCard;
